2f55ef187c2c297ad4f25b68048542c13ff13118
whatepa <jozef.grzesik239@gmail.com>
2025-06-05 00:09:28 +0200

    added categorical cm, updated cm

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 import math
+import numpy as np
+from scipy import stats
 
 app = Flask(__name__)
 
@@ -10,14 +12,16 @@ df = pd.read_csv('brain_stroke.csv')
 df_mapped = df.copy()
 df_mapped['stroke'] = df_mapped['stroke'].map({0: 'Brak udaru', 1: 'Udar'})
 
+numeric_df = df.select_dtypes(include=['number'])
+categorical_df = df_mapped.select_dtypes(include=['object', 'category', 'bool'])
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/dashboard_stats')
 def dashboardstats():
-  numeric_df = df.select_dtypes(include=['number'])
-  categorical_df = df_mapped.select_dtypes(include=['object', 'category', 'bool'])
+  
 
   numeric_stats = {}
   for column in numeric_df.columns:
@@ -105,11 +109,59 @@ def dashboard_cm():
         z=corr_matrix.values,
         colorscale='RdBu',
         zmin=-1,
-        zmax=1
+        zmax=1,
+        text=[[f'{val:.2f}' for val in row] for row in corr_matrix.values],
+        texttemplate='%{text}',
+        textfont={"size": 14},
     ))
+
+    fig.update_layout(
+        title='Korelacje między zmiennymi numerycznymi',
+        height=800,
+        xaxis_title='Zmienne',
+        yaxis_title='Zmienne'
+    )
     
     graph_html = fig.to_html(full_html=False)
     return render_template('dashboard_cm.html', graph_html=graph_html)
+
+@app.route('/dashboard_categorical')
+def dashboard_categorical():
+    
+    def cramers_v(confusion_matrix):
+        chi2 = stats.chi2_contingency(confusion_matrix)[0]
+        n = confusion_matrix.sum().sum()
+        min_dim = min(confusion_matrix.shape) - 1
+        return np.sqrt(chi2 / (n * min_dim))
+    
+    corr_matrix = pd.DataFrame(index=categorical_df.columns, columns=categorical_df.columns)
+    
+    for var1 in categorical_df:
+        for var2 in categorical_df:
+            confusion_matrix = pd.crosstab(df_mapped[var1], df_mapped[var2])
+            corr_matrix.loc[var1, var2] = cramers_v(confusion_matrix)
+    
+    fig = go.Figure(data=go.Heatmap(
+        z=corr_matrix.values,
+        x=categorical_df.columns,
+        y=categorical_df.columns,
+        colorscale='RdBu',
+        zmin=-1,
+        zmax=1,
+        text=[[f'{val:.2f}' for val in row] for row in corr_matrix.values],
+        texttemplate='%{text}',
+        textfont={"size": 14},
+    ))
+    
+    fig.update_layout(
+        title='Korelacje między zmiennymi kategorycznymi (V Cramera)',
+        height=800,
+        xaxis_title='Zmienne',
+        yaxis_title='Zmienne'
+    )
+    
+    graph_html = fig.to_html(full_html=False)
+    return render_template('dashboard_categorical.html', graph_html=graph_html)
 
 if __name__ == '__main__':
     app.run(debug=True)
